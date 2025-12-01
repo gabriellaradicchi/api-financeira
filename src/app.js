b599ff2f9a012544c9dab52fc57d7d48a4d45679
Gabriella Radicchi <gabriellaradicchi@hotmail.com>
2025-12-01 16:05:35 -0300

    Adicionando Frontend público

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const conn = require('./db/conn');
+const path = require('path'); 
+const conn = require('./db/conn'); 
 const routes = require('./routes/router'); 
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 conn();
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api', routes);
 
